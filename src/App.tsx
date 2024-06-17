79f6e55db68628c1d1d0b2ae02f9ffaa2624ab9f
tais defante <taisadefante@hotmail.com>
2024-06-17 10:19:30 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from "./styles/globo";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá!</h1>
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
